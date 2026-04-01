25c7ed8df5025cbafb9f06ecd5c349f0d20fbd79
frankmcsherry <fmcsherry@me.com>
2026-04-01 01:35:37 +0000

    Deploying to gh-pages from @ frankmcsherry/datatoad@d16572f2f5d8ae4c003ca0a72b298cfb8470c24c 🚀

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b0c0ab71.js',
+                path_to_root + 'searchindex-4e75cc64.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
